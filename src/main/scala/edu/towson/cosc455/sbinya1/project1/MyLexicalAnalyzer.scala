588c000d30a895ab394660951f4832919fc7e97b
ShirBin <sbinya1@students.towson.edu>
2016-10-30 12:50:49 -0400

    example on 2016-10-20

--- src/main/scala/edu/towson/cosc455/sbinya1/project1/MyLexicalAnalyzer.scala
+++ src/main/scala/edu/towson/cosc455/sbinya1/project1/MyLexicalAnalyzer.scala
@@ -3,12 +3,17 @@ package edu.towson.cosc455.sbinya1.project1
 /**
   * Created by uriel on 10/11/2016.
   */
-class MyLexicalAnalyzer extends LexicalAnalyzer{
+
+class MyLexicalAnalyzer extends LexicalAnalyzer {
   override def addChar(): Unit = ???
+
+  // override def lookup(): Boolean = ???
+
+  override def getNextToken(): Unit = {
+    val c  = getChar()
+  }
 
   override def getChar(): Char = ???
 
-  override def getNextToken(): Unit = ???
 
-  override def lookup(): Boolean = ???
 }
