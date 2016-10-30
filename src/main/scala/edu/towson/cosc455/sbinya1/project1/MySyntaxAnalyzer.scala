09e4ca57128d6b851126d4cd7a0cb97e166c5d4d
ShirBin <sbinya1@students.towson.edu>
2016-10-30 12:48:50 -0400

    example on 2016-10-20

--- src/main/scala/edu/towson/cosc455/sbinya1/project1/MySyntaxAnalyzer.scala
+++ src/main/scala/edu/towson/cosc455/sbinya1/project1/MySyntaxAnalyzer.scala
@@ -5,48 +5,41 @@ package edu.towson.cosc455.sbinya1.project1
   */
 class MySyntaxAnalyzer extends SyntaxAnalyzer{
   override def gittex(): Unit = {
-    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)) {
-      // add to parse tree
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)){
+      // add to parse tree / stack
       Compiler.Scanner.getNextToken()
-      variableDefine()
-      title()
-      body()
-      if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCE)) {
-        // do stuff
-      }
     }
-
     else {
-      println("SYNTAX ERROR")
+      println("Error")
       System.exit(1)
     }
   }
 
-  override def title(): Unit = ???
-
-  override def body(): Unit = ???
-
   override def paragraph(): Unit = ???
-
-  override def innerText(): Unit = ???
-
-  override def heading(): Unit = ???
-
-  override def variableDefine(): Unit = ???
-
-  override def variableUse(): Unit = ???
-
-  override def bold(): Unit = ???
-
-  override def italics(): Unit = ???
-
-  override def listItem(): Unit = ???
 
   override def innerItem(): Unit = ???
 
+  override def innerText(): Unit = ???
+
   override def link(): Unit = ???
+
+  override def italics(): Unit = ???
+
+  override def body(): Unit = ???
+
+  override def bold(): Unit = ???
+
+  override def newline(): Unit = ???
+
+  override def title(): Unit = ???
+
+  override def variableDefine(): Unit = ???
 
   override def image(): Unit = ???
 
-  override def newline(): Unit = ???
+  override def variableUse(): Unit = ???
+
+  override def heading(): Unit = ???
+
+  override def listItem(): Unit = ???
 }
