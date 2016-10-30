fcb186177d7ecc961f13f27592bc5a170fa4a2b3
ShirBin <sbinya1@students.towson.edu>
2016-10-30 12:44:01 -0400

    example on 2016-10-20

--- src/main/scala/edu/towson/cosc455/sbinya1/project1/CONSTANTS.scala
+++ src/main/scala/edu/towson/cosc455/sbinya1/project1/CONSTANTS.scala
@@ -4,8 +4,24 @@ package edu.towson.cosc455.sbinya1.project1
   * Created by uriel on 10/11/2016.
   */
 object CONSTANTS {
-  DOCB ::= ‘\BEGIN’
-  DOCE ::= '\END'
-  TITLEB ::= '\TITLE['
-  BRACKETE ::= ‘]’
+  val DOCB : String = "\\BEGIN"
+  val DOCE : String = "\\END"
+  val TITLEB : String = "\\TITLE["
+  val BRACKETE : String = "]"
+  val HEADING : String = "#"
+  val PARAB : String = "\\PARAB"
+  val PARAE : String = "\\PARAE"
+  val BOLD : String = "**"
+  val ITALICS : String = "*"
+  val LISTITEM : String = "+"
+  val NEWLINE : String = "\\"
+  val LINKB : String = "["
+  val ADDRESSB : String = "("
+  val ADDRESSE : String = ")"
+  val IMAGEB : String = "!["
+  val DEFB : String = "\\DEF["
+  val EQSIGN : String = "="
+  val USEB : String = "\\USE["
+  val REQTEXT : String = "Any plain text"
+  val TEXT : String = "Any plain text | ε"
 }
