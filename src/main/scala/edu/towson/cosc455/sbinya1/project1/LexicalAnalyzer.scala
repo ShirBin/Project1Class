829716f5828dbc326108b5a961d9c51b5223f961
ShirBin <sbinya1@students.towson.edu>
2016-10-30 12:50:03 -0400

    example on 2016-10-20

--- src/main/scala/edu/towson/cosc455/sbinya1/project1/LexicalAnalyzer.scala
+++ src/main/scala/edu/towson/cosc455/sbinya1/project1/LexicalAnalyzer.scala
@@ -1,8 +1,15 @@
 package edu.towson.cosc455.sbinya1.project1
 
 trait LexicalAnalyzer {
-  def addChar() : Unit
-  def getChar() : Char
-  def getNextToken() : Unit
-  def lookup() : Boolean
+  def addChar(): Unit
+
+  def getChar(): Char
+
+  def getNextToken(): Unit
+
+  def lookup(): Boolean = {
+    println("this is lookup implementation!")
+  }
+
 }
+
